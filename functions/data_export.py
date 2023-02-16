a9c9007e6ed1d2acf3efa0d29fc44e0294cbb192
Coralie <immacora@gmail.com>
2023-02-16 10:03:29 +0100

    Ajout de la gestion d'erreur et des indications utilisateur

--- functions/data_export.py
+++ functions/data_export.py
@@ -3,12 +3,20 @@ import csv
 
 
 def create_csv_result(csv_name, result):
-    """Crée le répertoire result s'il n'existe pas et y ajoute le fichier des résultats obtenus.
+    """Crée le répertoire result s'il n'existe pas et y ajoute le fichier du résultat obtenu.
     """
     if not os.path.exists('result'):
         os.makedirs('result')
-    keys = result[0].keys()
-    with open(os.path.join('result/') + csv_name + '.csv', 'w', encoding='utf-8', newline='') as csvfile:
-        dict_writer = csv.DictWriter(csvfile, keys, delimiter=';')
-        dict_writer.writeheader()
-        dict_writer.writerows(result)
+    
+    print(f"\nCréation du fichier {csv_name}.")
+
+    try:
+        keys = result[0].keys()
+    except KeyError:
+        print("ERREUR: La clé est est incorrecte ou inexistante dans la liste de résultats.")
+    else:
+        with open(os.path.join('result/') + csv_name + '.csv', 'w', encoding='utf-8', newline='') as csvfile:
+            dict_writer = csv.DictWriter(csvfile, keys, delimiter=';')
+            dict_writer.writeheader()
+            dict_writer.writerows(result)
+        print(f"\nLe fichier {csv_name} a bien été créé, il est consultable depuis le répertoire result.\n")
