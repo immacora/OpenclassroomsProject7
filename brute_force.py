fa68c66a55175d12afb551555f1d9a1b66a5bead
Coralie <immacora@gmail.com>
2023-06-20 20:06:07 +0200

    finalisation algorithme brute_force

--- brute_force.py
+++ brute_force.py
@@ -1,142 +1,72 @@
-from functions import data_import
-from functions import data_export
 import time
+import pandas as pd
+import itertools
+from data.data_functions import DATAFRAME_0, create_csv_result
 
 
-def combinations(stocks):
-    """Boucle sur la liste d'actions pour créer les premières combinaisons.
-    Boucle ensuite sur la liste des combinaisons pour créer les prochaines combinaisons
-    en y ajoutant chaque action qui suit la dernière action de la combinaison.
-
-    Retourne:
-        liste: Combinaisons d'actions (liste de listes).
+def brute_force(df):
     """
-    count = 0
-    combinations = []
-
-    try:
-        print("\nCréation des combinaisons d'actions.")
-
-        for i in range(0, len(stocks)):
-            combination = [stocks[i]]
-            combinations.append(combination)
-            count += 1
-
-            for j in range(0, (len(stocks) - count)):
-                combination = [stocks[i], stocks[j + count]]
-                combinations.append(combination)
-
-                for stock in stocks:
-                    if (stock == combination[-1]) and (combination[-1] != stocks[-1]):
-                        stock_index = stocks.index(stock)
-                        combination = combination + [stocks[stock_index + 1]]
-                        combinations.append(combination)
-        return combinations
-    except Exception as e:
-        print(f"\nERREUR: Les combinaisons d'actions n'ont pas pu être créées.\n{str(e)}")
-
-
-def create_combinations_result(combinations):
-    """Crée un dictionnaire par combinaison d'action contenant la simulation d'investissement et de profit
-    (id, name, len_combination, total_investment, total_profit) et la liste des dictionnaires.
-
-    Retourne:
-        liste: Résultats (dictionnaires) de la simulation d'investissement et de profit par combinaison d'actions.
-    """
-    combinations_result = []
-    count = 0
-
-    try:
-        print("\nCréation de la simulation d'investissement et de profit par combinaison d'actions.")
-
-        for combination in combinations:
-            count += 1
-            id = count
-            name = f"combinaison_{count}"
-            len_combination = len(combination)
-            total_investment = 0.0
-            total_profit = 0.0
-
-            combination_result = {
-                'id': id,
-                'name': name,
-                'actions_combination': combination,
-                'nb_actions': len_combination,
-            }
-
-            for action in combination:
-                action_price = float(action[1])
-                total_investment += action_price
-
-                action_profit = float(action[2])
-                total_profit += action_profit
-
-                combination_result['total_investment'] = total_investment
-                combination_result['total_profit'] = total_profit
-
-            combinations_result.append(combination_result)
-
-        return combinations_result
-    except Exception as e:
-        print(f"\nERREUR: Le calcul de la simulation a échoué.\n{str(e)}")
-
-
-def select_optimal_investment(combinations_result):
-    """Trie les dictionnaires de chaque simulation financière et sélectionne celui qui offre
-    le meilleur profit pour un montant maximal de 500 euros.
-    Supprime les investissements de plus de 500€ puis effectue un tri par sélection pour trouver le profit maximal.
-
-    Retourne:
-        dict: Combinaison optimale détaillée.
-    """
-    combinations_max_invest = []
-
-    try:
-        print("\nSélection du meilleur profit pour un montant maximal de 500 euros.")
-
-        for combination in combinations_result:
-            if combination['total_investment'] <= 500.0:
-                combinations_max_invest.append(combination)
-
-        combination_total_profit_max = combinations_max_invest[0]['total_profit']
-        optimal_investment = combinations_max_invest[0]
-
-        for combination in combinations_max_invest:
-            if combination['total_profit'] > combination_total_profit_max:
-                combination_total_profit_max = combination['total_profit']
-                optimal_investment = combination
-
-        return optimal_investment
-    except Exception as e:
-        print(f"\nERREUR: La sélection du meilleur profit a échoué.\n{str(e)}")
-
-
-def brute_force():
-    """Algorithme de Force brute énumérant toutes les combinaisons possibles d'une liste d'actions
+    Algorithme de Force brute énumérant toutes les combinaisons possibles d'une liste d'actions
     et sélectionnant celle qui totalise le meilleur profit sur 2 ans selon certains critères:
     - Chaque action ne peut être achetée qu'une seule fois.
     - Il est impossible d'acheter une fraction d'action.
     - L'investissement maximal est de 500 euros.
-    Crée la liste des dictionnaires de chaque simulation financière par combinaison
-    et l'exporte dans le répertoire result.
-    Trie les dictionnaires, sélectionne la solution optimale selon les critères énoncés et l'affiche.
+
+    Itère sur le nombre d'actions de la liste pour créer, a chaque tour,
+    la liste des combinaisons possibles de longueur i avec la fonction combinations du module itertools.
+        Crée 1 dictionnaire pour chaque combinaison de la liste (+nombre d'actions, investissement et profit total).
+        Ajoute chaque dictionnaire à la liste totale des combinaisons pour export en csv.
+        Filtre les résultats par montant d'investissement (max: 500 euros).
+        Sélectionne la meilleure combinaison par comparaison de profit max.
+    Résultat :
+        csv : liste de toutes les combinaisons
+        csv : investissement optimal
     """
+    total_combinations = []
+    optimal_investment = {}
+    total_investment_max = 500.0
+    total_profit_max = 0.0
+
     try:
-        print("\nDébut de la spéculation par algorithme de Force brute.")
+        stocks = df.values.tolist()
+        print("\nListe des actions à analyser :\n", df,
+              "\n\nDébut du calcul par algorithme de Force brute.")
         start_time = time.time()
 
-        stocks = data_import.get_stocks()
+        for i in range(1, len(stocks)+1):
+            combinations = itertools.combinations(stocks, i)
 
-        total_combinations = combinations(stocks)
+            for combination in combinations:
+                nb_stocks = len(combination)
+                total_investment = sum(item[1] for item in combination)
+                total_profit = sum(item[2] for item in combination)
 
-        combinations_result = create_combinations_result(total_combinations)
-        csv_name = 'brute_force_combinations_result'
-        data_export.create_csv_result(csv_name, combinations_result)
+                combination_dict = {
+                    'stocks_combination': list(combination),
+                    'nb_stocks': nb_stocks,
+                    'total_investment': total_investment,
+                    'total_profit': total_profit
+                }
+                total_combinations.append(combination_dict)
 
-        optimal_investment = [select_optimal_investment(combinations_result)]
-        csv_name = 'brute_force_optimal_investment'
-        data_export.create_csv_result(csv_name, optimal_investment)
-        print(f"L'investissement optimal selon l'algorithme de force brute est :\n{optimal_investment}")
+                if total_investment <= total_investment_max:
+                    if total_profit > total_profit_max:
+                        optimal_investment = combination_dict
+                        total_profit_max = total_profit
+
+        df_total_combinations = pd.DataFrame(total_combinations)
+        csv_name = "brute_force_total_combinations"
+        create_csv_result(csv_name, df_total_combinations)
+
+        df_optimal_investment = pd.DataFrame(optimal_investment)
+        csv_name = "BRUTE_FORCE_optimal_investment"
+        create_csv_result(csv_name, df_optimal_investment)
+
+        print(f"\nL'investissement optimal selon l'algorithme de force brute est :\
+              \nCombinaison: {optimal_investment['stocks_combination']}\
+              \nInvestissement total: {optimal_investment['total_investment']}\
+              \nProfit total: {optimal_investment['total_profit']}"
+              )
 
         end_time = time.time()
         run_time = end_time - start_time
@@ -146,5 +76,5 @@ def brute_force():
         print(f"\nERREUR: L'algorithme n'a pas fonctionné.\n{str(e)}")
 
 
-""" Appel de l'algorithme BRUTE FORCE"""
-brute_force()
+if __name__ == "__main__":
+    brute_force(DATAFRAME_0)
